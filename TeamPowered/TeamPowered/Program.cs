dfcdc1eb7fb70bfb763fe1da94e537e7ec97d908
Cristian Lazurean <cristian_lazurean@hotmail.com>
2018-03-03 23:18:23 +0200

    Added new person

--- TeamPowered/TeamPowered/Program.cs
+++ TeamPowered/TeamPowered/Program.cs
@@ -10,7 +10,7 @@ namespace TeamPowered
     {
         static void Main(string[] args)
         {
-            var loPersons = new List<string> { "qwerty", "asdfgh" };
+            var loPersons = new List<string> { "qwerty", "asdfgh", "zxcvbn" };
         }
     }
 }
